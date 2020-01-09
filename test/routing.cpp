6c50d809b97fd06efb649e483dd49870b4ae296d
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-01-09 18:08:16 +0200

    Support for nested routers

---
+++ test/routing.cpp
@@ -0,0 +1,59 @@
+#include <ArduinoUnitTests.h>
+#include "../src/aWOT.h"
+#include "./mocks/MockStream.h"
+
+void beforeAll(Request &req, Response &res) {
+  res.print("beforeAll-");
+}
+
+void beforeRouter(Request &req, Response &res) {
+  res.print("beforeRouter-");
+}
+
+void handler(Request &req, Response &res) {
+  char test1[10];
+  req.route("test1", test1, 10);
+  res.print(test1);
+}
+
+void afterRouter(Request &req, Response &res) {
+  res.print("-afterRouter");
+}
+
+void afterAll(Request &req, Response &res) {
+  res.print("-afterAll");
+}
+
+unittest(nested_routers) {
+  char const *request = 
+   "GET /router1/router2/router3/route/test HTTP/1.0" CRLF
+  CRLF;
+
+  char const *expected = 
+  "HTTP/1.1 200 OK" CRLF 
+  "Content-Type: text/plain" CRLF
+  "Connection: close" CRLF 
+  CRLF 
+  "beforeAll-beforeRouter-test-afterRouter-afterAll";
+
+  MockStream stream(request);
+  Router router1("/router1");
+  Router router2("/router2");
+  Router router3("/router3"); 
+  Application app;
+
+  router3.use(&beforeRouter);
+  router3.get("/route/:test1", &handler);
+  router3.use(&afterRouter);
+  router2.route(&router3);
+  router1.route(&router2);
+
+  app.use(&beforeAll);
+  app.route(&router1);
+  app.use(&afterAll);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest_main()
