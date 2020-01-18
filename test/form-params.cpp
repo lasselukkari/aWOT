22c21594deeb2fc8ff169b561b4af030f7838765
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-01-18 16:45:46 +0200

    Tests for form params reading

--- test/form-params.cpp
+++ test/form-params.cpp
@@ -2,49 +2,51 @@
 #include "../src/aWOT.h"
 #include "./mocks/MockStream.h"
 
-void handler(Request &req, Response &res) {
-  char test1[6];
-  char test2[6];
+void handler(Request & req, Response & res) {
+  char name[6];
+  char value[6];
 
-  if (!req.query("test1", test1, 6)) {
-    return res.sendStatus(400);
+  while (req.left()) {
+    if(!req.form(name, 6, value, 6)) {
+      return res.sendStatus(400);
+    }
+
+    res.print(name);
+    res.print(": ");
+    res.println(value);
   }
-
-  if (!req.query("test2", test2, 6)) {
-    return res.sendStatus(400);
-  }
-
-  res.set("Content-Type", "text/plain");
-  res.print(test1);
-  res.print(" ");
-  res.print(test2);
 }
 
-unittest(query_params_ok) {
+unittest(form_params) {
   char const *request =
-    "GET /query?test1=test1&test2=test2 HTTP/1.0" CRLF
-    CRLF;
+    "POST / HTTP/1.0" CRLF
+    "Content-Length: 23" CRLF
+    CRLF
+    "test1=test1&test2=test2";
 
   char const *expected =
     "HTTP/1.1 200 OK" CRLF
     "Content-Type: text/plain" CRLF
     "Connection: close" CRLF
     CRLF
-    "test1 test2";
-
+    "test1: test1" CRLF
+    "test2: test2" CRLF;
+    
   MockStream stream(request);
   Application app;
 
-  app.get("/query", &handler);
+  app.post("/", &handler);
   app.process(&stream);
 
   assertEqual(expected, stream.response());
 }
 
-unittest(query_params_value_too_long) {
+unittest(form_params_name_too_long) {
   char const *request =
-    "GET /query?test1=123456&test2=test2 HTTP/1.0" CRLF
-    CRLF;
+    "POST / HTTP/1.0" CRLF
+    "Content-Length: 23" CRLF
+    CRLF
+    "test1*=test1&test2=test2";
 
   char const *expected =
     "HTTP/1.1 400 Bad Request" CRLF
@@ -52,35 +54,34 @@ unittest(query_params_value_too_long) {
     "Connection: close" CRLF
     CRLF
     "Bad Request";
-
+    
   MockStream stream(request);
   Application app;
 
-  app.get("/query", &handler);
+  app.post("/", &handler);
   app.process(&stream);
 
   assertEqual(expected, stream.response());
 }
 
-unittest(router_query_params) {
+unittest(form_params_value_too_long) {
   char const *request =
-    "GET /router/query?test1=test1&test2=test2 HTTP/1.0" CRLF
-    CRLF;
+    "POST / HTTP/1.0" CRLF
+    "Content-Length: 23" CRLF
+    CRLF
+    "test1=test1*&test2=test2";
 
   char const *expected =
-    "HTTP/1.1 200 OK" CRLF
+    "HTTP/1.1 400 Bad Request" CRLF
     "Content-Type: text/plain" CRLF
     "Connection: close" CRLF
     CRLF
-    "test1 test2";
-
+    "Bad Request";
+    
   MockStream stream(request);
-  Router router("/router");
   Application app;
 
-  router.get("/query", &handler);
-  app.route(&router);
-
+  app.post("/", &handler);
   app.process(&stream);
 
   assertEqual(expected, stream.response());
