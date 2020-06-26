34aa9cecc0453ef417a1f06430dd480ff6c0aaa2
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-06-26 15:26:27 +0300

    Handle 1xx status responses correctly

---
+++ test/statuses.cpp
@@ -0,0 +1,38 @@
+#include <ArduinoUnitTests.h>
+#include "../src/aWOT.h"
+#include "./mocks/MockStream.h"
+
+void handler(Request & req, Response & res) {
+  res.set("Before-Continue", "TestBefore");
+  res.status(100);
+  res.set("After-Continue", "TestAfter");
+  res.sendStatus(204);
+}
+
+unittest(status_continue) {
+  char const *request =
+    "POST / HTTP/1.1" CRLF
+    "Expect: 100-continue" CRLF
+    "Content-Length: 4" CRLF
+    CRLF
+    "test";
+
+  char const *expected =
+    "HTTP/1.1 100 Continue" CRLF
+    "Before-Continue: TestBefore" CRLF
+    CRLF
+    "HTTP/1.1 204 No Content" CRLF
+    "After-Continue: TestAfter" CRLF
+    "Connection: close" CRLF
+    CRLF;
+
+  MockStream stream(request);
+  Application app;
+
+  app.post("/", &handler);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest_main()
