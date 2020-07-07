67b5056df30ac29e7586b0065e156d48c9664786
Lasse Lukkari <lasse.lukkari@gmail.com>
2020-07-07 11:46:54 +0300

    Add more routing tests

--- test/routing.cpp
+++ test/routing.cpp
@@ -56,4 +56,100 @@ unittest(nested_routers) {
   assertEqual(expected, stream.response());
 }
 
+void testHandler(Request &req, Response &res) {
+  res.print("test");
+}
+
+unittest(get_route) {
+  char const *request =
+    "GET /whatever HTTP/1.0" CRLF
+    CRLF;
+
+  char const *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "test";
+
+  MockStream stream(request);
+  Application app;
+
+
+  app.get(&testHandler);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest(use_route_with_path) {
+  char const *request =
+    "POST /usepath HTTP/1.0" CRLF
+    CRLF;
+
+  char const *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "test";
+
+  MockStream stream(request);
+  Application app;
+
+
+  app.use("/usepath", &testHandler);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest(router_without_path) {
+  char const *request =
+    "GET /route HTTP/1.0" CRLF
+    CRLF;
+
+  char const *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "test";
+
+  MockStream stream(request);
+  Application app;
+  Router router;
+
+
+  router.get("/route", &testHandler);
+  app.use(&router);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest(router_handler_without_path) {
+  char const *request =
+    "GET /router/whatever HTTP/1.0" CRLF
+    CRLF;
+
+  char const *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "test";
+
+  MockStream stream(request);
+  Application app;
+  Router router;
+
+
+  router.get(&testHandler);
+  app.use("/router", &router);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
 unittest_main()
