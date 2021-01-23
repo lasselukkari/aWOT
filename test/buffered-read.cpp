c55ae56976f2f3fa35bb35edcc11284d6396b99d
Lasse Lukkari <lasse.lukkari@gmail.com>
2021-01-23 12:43:59 +0200

    Enable buffered reads

---
+++ test/buffered-read.cpp
@@ -0,0 +1,36 @@
+#include <ArduinoUnitTests.h>
+#include "../src/aWOT.h"
+#include "./mocks/MockStream.h"
+
+void handler(Request & req, Response & res) {
+  uint8_t buffer[100] = {};
+
+  int read = req.read(buffer , 100);
+
+  res.write(buffer, read);
+}
+
+unittest(buffered_read) {
+  const char *request =
+    "POST / HTTP/1.0" CRLF
+    "Content-Length: 4" CRLF
+    CRLF
+    "body";
+
+  const char *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "body";
+    
+  MockStream stream(request);
+  Application app;
+
+  app.post("/", &handler);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+unittest_main()
