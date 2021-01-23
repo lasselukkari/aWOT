532fba2741cf0a0c820ab7b2d543790a8ab9fd58
Lasse Lukkari <lasse.lukkari@gmail.com>
2021-01-23 15:01:58 +0200

    Add test for buffered read return value

--- test/buffered-read.cpp
+++ test/buffered-read.cpp
@@ -10,7 +10,40 @@ void handler(Request & req, Response & res) {
   res.write(buffer, read);
 }
 
+void readCountHandler(Request & req, Response & res) {
+  uint8_t buffer[100] = {};
+  req.push(req.read());
+
+  int read = req.read(buffer , 100);
+
+  res.print(read);
+}
+
 unittest(buffered_read) {
+  const char *request =
+    "POST / HTTP/1.0" CRLF
+    "Content-Length: 4" CRLF
+    CRLF
+    "body";
+
+  const char *expected =
+    "HTTP/1.1 200 OK" CRLF
+    "Content-Type: text/plain" CRLF
+    "Connection: close" CRLF
+    CRLF
+    "4";
+    
+  MockStream stream(request);
+  Application app;
+
+  app.post("/", &readCountHandler);
+  app.process(&stream);
+
+  assertEqual(expected, stream.response());
+}
+
+
+unittest(buffered_read_count) {
   const char *request =
     "POST / HTTP/1.0" CRLF
     "Content-Length: 4" CRLF
